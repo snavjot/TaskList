7512c9df72cf9d6978114299ba33336ac314881e
Navjot Singh <navjotsinghgr8@gmail.com>
2016-04-08 15:21:26 +0530

    Adding a fliestorage component

---
+++ app/src/main/java/com/navjot/storage/FileStorageHelper.java
@@ -0,0 +1,34 @@
+package com.navjot.storage;
+
+import java.io.FileOutputStream;
+
+/**
+ * Created by navsin on 3/29/2016.
+ */
+public class FileStorageHelper {
+
+    private FileOutputStream outputStream;
+
+    public FileStorageHelper(FileOutputStream outputStream) {
+        this.outputStream = outputStream;
+    }
+
+    public void writeCSVString(String csvString) {
+        try {
+            outputStream.write(csvString.getBytes());
+        } catch (Exception ex) {
+
+        }
+    }
+
+    
+
+    public void closeStream() {
+        try {
+            outputStream.close();
+        }
+        catch (Exception ex) {
+
+        }
+    }
+}
